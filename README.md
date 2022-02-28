8de1cb5b2ddc30f42ce66963ac890f3434143b74
rebeccawong99 <47613304+rebeccawong99@users.noreply.github.com>
2022-02-28 10:20:38 -0800

    adding again

---
+++ README.md
@@ -0,0 +1,5 @@
+# DFS3
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/rebeccawong/pen/NWwaqza](https://codepen.io/rebeccawong/pen/NWwaqza).
+
+
--- script.js
+++
@@ -1,9 +0,0 @@
-var redball = document.querySelector(".redball");
-
-window.addEventListener("mousemove" , event => {
-  var x = event.clientX;
-  var y = event.clientY;
-  
-  redball.style.top = y + "px";
-  redball.style.left = x + "px";
-});
